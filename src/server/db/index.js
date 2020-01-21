e7c98b528b08c8f7bf6cc829a9b84a5eed33b1d5
misila <karine.fontaine@gmail.com>
2020-01-21 14:49:00 +0100

    cleaning directories

--- src/server/db/index.js
+++
@@ -1,8 +0,0 @@
-import * as mysql from 'mysql';
-import config from '../config';
-
-export const Connection = mysql.createConnection(config.mysql);
-
-Connection.connect(err => {
-    if (err) console.log(err);
-});
