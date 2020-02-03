18b1c2bcfa8f13ea39f532227f6e29655b39b615
misila <karine.fontaine@gmail.com>
2020-02-03 10:33:24 +0100

    Update Navigation.js into react Hooks

--- client/src/Navigation.js
+++ client/src/Navigation.js
@@ -2,34 +2,28 @@ import React, { Component } from 'react';
 import { FaUsers } from 'react-icons/fa';
 import { IoIosPower } from 'react-icons/io';
 
-class Navigation extends Component {
+function Navigation (props) {
 
-    render () {
-
-        const { user, matricule } = this.props;
-        console.log('Navigation ===>  user:', user, ', matricule:', matricule);
-        
-        return (
-            <div className="siteNav family-sans navbar navbar-expand bg-primary navbar-dark higher">
-                <div className="container-fluid">
-                    <a href="/" className="navbar-brand ">
-                        <FaUsers className="mr-1" /> Bienvenu(e)  { user } &nbsp; 
-                        <IoIosPower className="mr-1" />
-                    </a>
-                    <div className="navbar-nav mt-auto">
-                        <a className="nav-item nav-link" href="/compte"> Mon Compte </a>
-                        <a className="nav-item nav-link" href="/reservation"> Mes Réservations </a>
-                        <a className="nav-item nav-link" href="/outils"> Nos Outils</a>
-                        <a className="nav-item nav-link" href="/reserver"> Réserver </a>
-                        <a className="nav-item nav-link" href="/annuler" > Annuler </a>
-                        <a className="nav-item nav-link" href="/incident" > Ouvrir un incident </a>
-                        <a className="nav-item nav-link" href="/idees" >Boite à idées </a>
-                        <a className="nav-item nav-link" href="/reparerDvd"> Réparer des DVD </a>
-                        <a className="nav-item nav-link" href="/contact" > Contact </a>
-                    </div>
+    return (
+        <div className="siteNav family-sans navbar navbar-expand bg-primary navbar-dark higher">
+            <div className="container-fluid">
+                <a href="/" className="navbar-brand ">
+                    <FaUsers className="mr-1" /> Bienvenu(e)  { props.user } &nbsp; 
+                    <IoIosPower className="mr-1" />
+                </a>
+                <div className="navbar-nav mt-auto">
+                    <a className="nav-item nav-link" href="/compte"> Mon Compte </a>
+                    <a className="nav-item nav-link" href="/reservation"> Mes Réservations </a>
+                    <a className="nav-item nav-link" href="/outils"> Nos Outils</a>
+                    <a className="nav-item nav-link" href="/reserver"> Réserver </a>
+                    <a className="nav-item nav-link" href="/annuler" > Annuler </a>
+                    <a className="nav-item nav-link" href="/incident" > Ouvrir un incident </a>
+                    <a className="nav-item nav-link" href="/idees" >Boite à idées </a>
+                    <a className="nav-item nav-link" href="/reparerDvd"> Réparer des DVD </a>
+                    <a className="nav-item nav-link" href="/contact" > Contact </a>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
  export default Navigation;
--- server/server.js
+++ server/server.js
@@ -10,9 +10,8 @@ const generator = require('generate-password');
 // const nodeMailer = require('nodemailer');
 
 const app = express();
-console.log('dirname = ', __dirname);
+
 app.use(express.static(path.join(__dirname, 'build')));
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 
@@ -41,7 +40,6 @@ app.post('/renewpasswd', function (request, response) {
 
     console.log('newpassword = ' + newpassword);
     var md5newPassword = md5(newpassword);
-    console.log('md5newPassword = ' +md5newPassword);
 
     db.query("update user set PasswdMD5 = ? where Matricule= ?",
             [md5newPassword, matricule],
@@ -49,19 +47,14 @@ app.post('/renewpasswd', function (request, response) {
 
                 if (err) {
                     response.send('RenewPassword has failed ');
-                    //throw err;
                 }
-                console.log('err = ' + err);
-                console.log('rows = ' + JSON.stringify(rows));
                 response.send('renewpassword OK : ' + newpassword);
     });
-
-    //db.query
+    
 });
 
 app.post('/login', function (request, response) {
 
-    console.log('app.post(/login) , matricule = ', request.body.matricule);
     db.query('select Nom as nom, Prenom as prenom, Matricule as mat, PasswdMD5 as passwd from user where Matricule= ?',
             [request.body.matricule],
             function(err, rows, fields){
@@ -77,7 +70,6 @@ app.post('/login', function (request, response) {
                     var md5request = md5(request.body.password);
 
                     if (md5request === passwd) {
-                        console.log('send {',matricule, '} ');
                         response.send({ matricule, nom, prenom });
                     } else {
                         response.send({ error: 'Bad Password'});
@@ -86,17 +78,6 @@ app.post('/login', function (request, response) {
               });
 
 });
-
-/*
-app.get('/', function(request, response) {
-
-    db.query('select * from userds', function(err, rows, fields){
-
-         response.writeHead(200, {'Content-Type': 'application/json'});
-         response.end(JSON.stringify(rows));
-
-    });
-});*/
 
 
 app.listen(port, () => console.log(`Listening on port ${port}` ));
